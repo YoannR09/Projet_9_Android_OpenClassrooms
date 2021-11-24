74300441911eac11d3155bbf5430ca4a62e3960d
yoann roche <el-rambo-poto@hotmail.fr>
2021-11-24 08:55:53 +0100

    Fix bug, set text with int try to find resource with int id

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -29,6 +29,6 @@ public class MainActivity extends AppCompatActivity {
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(Integer.toString(quantity));
     }
 }
